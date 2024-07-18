cb1c93858122bae8576d28d17a4bbaf7c9644381
Perkles <davifernandosousadias18@gmail.com>
2024-07-18 19:13:33 -0300

    feat: added creation fields to the product form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl ,ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('',{
+    nonNullable:true,
+    validators: Validators.required,})
+  })
 
+  public onSubmit(){
+    this.form.controls.title;
+  }
 }
+
